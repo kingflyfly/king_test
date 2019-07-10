1e1dbc10e4b5dea9f39ffb02ab41d1afbb0c2778
kingflyfly <542001608@qq.com>
2019-07-10 14:47:42 +0800

    init

--- 选择排序.py
+++ 选择排序.py
@@ -1,9 +1,16 @@
 # 选择排序
-test_list = [5,4,2,1,3]
+test_list = [5,4,2,9,3,6]
 i = 0
 while i < len(test_list) - 1:
-    for j in range(i+1,len(test_list)):
-        if test_list[i] > test_list[j]:
-            test_list[i],test_list[j] = test_list[j],test_list[i]
+    minIndex = i
+    j = i + 1
+    while j < len(test_list):
+        if test_list[j] < test_list[minIndex]:
+            minIndex = j
+        j += 1
+    if minIndex != i:
+        temp = test_list[i]
+        test_list[i] = test_list[minIndex]
+        test_list[minIndex] = temp
     i += 1
 print(test_list)
