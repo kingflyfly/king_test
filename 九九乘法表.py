912d18cedae7b1be542db1b055fc4f5e1ff383b6
root <542001608@qq.com>
2019-12-30 20:31:34 +0800

    --

--- 九九乘法表.py
+++
@@ -1,9 +0,0 @@
-# 九九乘法表
-def jiujiu():
-    for i in range(1,10):
-        for j in range(1,i+1):
-            print(j,"*",i,"=",i * j," ",end=" ")
-        print()
-
-if __name__ == "__main__":
-    jiujiu()
--- 冒泡排序.py
+++
@@ -1,9 +0,0 @@
-#冒泡排序
-a = [5,4,2,1,3]
-j = 0
-while j < len(a) - 1:
-    for i in range(len(a)-1-j):
-        if a[i] > a[i + 1]:
-            a[i],a[i + 1] = a[i + 1],a[i]
-    j += 1
-print(a)
